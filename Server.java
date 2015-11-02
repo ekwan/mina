c023e362592fb3d81ccca343ece9604987500c52
Eugene E. Kwan <ekwan16@gmail.com>
2015-11-01 19:10:49 -0500

    configured to use a thread pool

--- Server.java
+++ Server.java
@@ -8,6 +8,7 @@ import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;  
 import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;  
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;  
+import org.apache.mina.filter.executor.ExecutorFilter;
 
 public class Server
 {  
@@ -23,10 +24,14 @@ public class Server
         for (int i=0; i < NUMBER_OF_UNITS; i++)
             WORK_LIST.add(new DummyRemoteWorkUnit((long)i));
 
+        // use a thread pool
+        ExecutorFilter executor = new ExecutorFilter(2, 4);  // number of threads to start with, max number of threads
+
         IoAcceptor acceptor = new NioSocketAcceptor();  
         acceptor.getSessionConfig().setReadBufferSize(2048);  
         acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);  
         acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new ObjectSerializationCodecFactory()));  
+        acceptor.getFilterChain().addLast("executor1", executor);
         acceptor.setHandler(new ServerHandler());  
         acceptor.bind(new InetSocketAddress(bindPort));  
           
